b2766e4376dd1e2b184d003717a074337b959d10
Loop1397 <shwlsdn0906@gmail.com>
2024-02-28 23:14:15 +0900

    jwt 토큰을 이용하기 위한 module설정 및 jwtService에 jwtService주입

--- back-end/src/user/user.module.ts
+++ back-end/src/user/user.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
+  imports: [
+    //passport 미들웨어를 사용하기 위한 passport 라이브러리 등록
+    PassportModule.register({ defaultStrategy: 'jwt'}),
+    JwtModule.register({
+      // jwt 토큰 발행시에 들어가는 secret 문자열
+      secret: 'Secret1234',
+      signOptions: {
+        //토큰이 유효한 시간 옵션(초 단위)
+        expiresIn: 60 * 60,
+      }
+    })
+  ],
   controllers: [UserController],
   providers: [UserService]
 })
--- back-end/src/user/user.service.ts
+++ back-end/src/user/user.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
+    constructor(
+        private jwtService: JwtService
+    ){}
     
     //비밀번호 암호화 함수
     async hashPassword(password: string) {
