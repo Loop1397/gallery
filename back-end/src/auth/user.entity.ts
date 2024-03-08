0a3b59caf0f8f4edae8286696dcab7bf4cb9deea
Loop1397 <shwlsdn0906@gmail.com>
2024-03-08 23:05:53 +0900

    user와 board 엔티티간의 관계 형성

--- back-end/src/auth/user.entity.ts
+++ back-end/src/auth/user.entity.ts
@@ -1,4 +1,5 @@
-import { BaseEntity, Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, Unique } from "typeorm";
+import { Board } from "src/board/board.entity";
+import { BaseEntity, Column, CreateDateColumn, DeleteDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, Unique } from "typeorm";
 
 // @Unique 데코레이터 : 필드 중 임의로 유니크한 값 지정 가능
 // 유니크한 값으로 지정된 컬럼에 동일한 값이 들어오려고 하면 500에러 발생
@@ -13,6 +14,12 @@ export class User extends BaseEntity {
 
     @Column()
     password: string;
+
+    // user와 board의 관계성을 만들어주기위한 컬럼
+    // 한 user가 여러 board를 만들 수 있기에 OneToMany Relationship
+    // eager가 true면 user entity를 가져올 때 board entity도 같이 가져옴 
+    @OneToMany(type => Board, board => board.user, { eager: true})
+    boards: Board[];
 
     // DB에 생성된 시간
     @CreateDateColumn({ name: 'created_at' })
--- back-end/src/board/board.entity.ts
+++ back-end/src/board/board.entity.ts
@@ -1,5 +1,6 @@
-import { BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { BaseEntity, Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { BoardStatus } from "./board.model";
+import { User } from "src/auth/user.entity";
 
 @Entity()
 export class Board extends BaseEntity {
@@ -17,6 +18,12 @@ export class Board extends BaseEntity {
 
     @Column()
     status: BoardStatus;
+
+    // user와 board의 관계성을 만들어주기위한 컬럼
+    // 한 user가 여러 board를 만들 수 있기에 OneToMany Relationship
+    // eager가 true면 user entity를 가져올 때 board entity도 같이 가져옴 
+    @ManyToOne(type => User, user => user.boards, {eager: false})
+    user: User;
 
     //DB에 들어가는 시간
     @CreateDateColumn({ name: 'created_at' })
